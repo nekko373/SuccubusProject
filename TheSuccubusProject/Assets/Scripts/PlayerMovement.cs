f71c3ab1017bbbc97c072c9bab7beb2ba091e96a
nekko373 <haveyouseensasha6@gmail.com>
2020-08-14 16:56:14 +0800

    Player Movement Added!
    in this commit, i added prefab of player with movment component. also added a sprite shape of the ground, and its component such as colliders and such....

---
+++ TheSuccubusProject/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+    float horizontalMove = 0f;
+    public float runSpeed = 40f;
+    // Update is called once per frame
+    void Update()
+    {
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        Debug.Log(horizontalMove);
+
+    }
+
+    void FixedUpdate() {
+
+        
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+
+
+
+    }
+
+
+
+
+}
