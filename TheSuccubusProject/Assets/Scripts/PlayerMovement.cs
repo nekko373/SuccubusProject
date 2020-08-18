058d066a5174b5c40548235348434c00395704cb
nekko373 <haveyouseensasha6@gmail.com>
2020-08-18 18:02:26 +0800

    Jumping function
    added a jumping function

--- TheSuccubusProject/Assets/Scripts/PlayerMovement.cs
+++ TheSuccubusProject/Assets/Scripts/PlayerMovement.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Animator animator;
-    public Rigidbody2D rigidbody2D;
-    public CharacterController2D controller;
-    float horizontalMove = 0f;
-    float verticalMove = 0f;
-    public float runSpeed = 40f;
-    public HalfBody halfBody;
-    bool halfBodyMode = false;
-    public CharacterScript characterScript;
+    public Animator animator; //reference to animator
+    public Rigidbody2D rigidbody2D; // reference to rigidbody of player
+    public CharacterController2D controller; //reference to the controller script
+    float horizontalMove = 0f; //horizontal move variable
+    float verticalMove = 0f; // vertical move variable
+    public float runSpeed = 40f; //run speed
+    public HalfBody halfBody; // reference to half body game objecct
+    bool halfBodyMode = false; //boolean for determining if half body mode is on
+    public CharacterScript characterScript; //reference to character script
+    bool jump = false; // boolean to determine if player is jumping
+    
     // Update is called once per frame
+    
     void Update()
     {
         //get movement
@@ -23,11 +26,10 @@ public class PlayerMovement : MonoBehaviour
         //animator
         animator.SetFloat("isWalking", Mathf.Abs(horizontalMove));
         
-        //check the horizontal move
-        Debug.Log("Horizontal: "+horizontalMove);
+    
         
         //detach body
-        if (Input.GetKeyDown("space")) {
+        if (Input.GetKeyDown(KeyCode.L)) {
 
         
             halfBody.HalfMode();
@@ -37,19 +39,23 @@ public class PlayerMovement : MonoBehaviour
 
         }
         //release detach
-      else if (Input.GetKeyUp("space")) {
+      else if (Input.GetKeyUp(KeyCode.L)) {
             
             halfBodyMode = false;
-            Debug.Log("Hi " + halfBodyMode);
+        
             animator.SetBool("isDetached", false);
             halfBody.ReturnBody();
 
         }
         if (Input.GetButtonDown("takedmg")) {
-            Debug.Log("Damage Taken: " + 10);
+        
             characterScript.TakeDamage(10);
 
         }
+
+
+        //check if lose and acquire life force is working
+        /*
         if (Input.GetButtonDown("takelf"))
         {
             Debug.Log("LF Taken: " + 10);
@@ -63,24 +69,45 @@ public class PlayerMovement : MonoBehaviour
             characterScript.loseLifeForce(10);
 
         }
+        */
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            
+            jump = true;
+            
+            animator.SetBool("IsJumping", true);
+            
+
+            
+        }
+
     }
+    void OnLanding() {
+
+
+        animator.SetBool("IsJumping", false);
+
+    }
+
 
     void FixedUpdate() {
 
         if (halfBodyMode == true) {
 
-            Debug.Log("Vertical: " + verticalMove);
-            Debug.Log("Half body mode is true: " + halfBodyMode);
+           // Debug.Log("Vertical: " + verticalMove);
+            //Debug.Log("Half body mode is true: " + halfBodyMode);
             controller.Move(horizontalMove * Time.fixedDeltaTime, verticalMove * Time.fixedDeltaTime, true, false);
-
+            
 
 
         }
         else if(halfBodyMode == false) {
 
-            Debug.Log("Half body mode is false: " + halfBodyMode);
-            controller.Move(horizontalMove * Time.fixedDeltaTime, verticalMove * Time.fixedDeltaTime, false, false);
-
+            //Debug.Log("Half body mode is false: " + halfBodyMode);
+            controller.Move(horizontalMove * Time.fixedDeltaTime, verticalMove * Time.fixedDeltaTime, false, jump);
+            Debug.Log("jump should be true:  " + jump);
+            jump = false;
+            Debug.Log("jump is now " + jump);
         }
 
     }
